0b63292603be72021e48e02a10a8df19320825cd
Jonghyun Kim <jhkim@dit.ac.kr>
2019-11-04 22:03:43 +0900

    Initial Commit

---
+++ Simple CoreLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple CoreLocation
+//
+//  Created by 김종현 on 2019/11/04.
+//  Copyright © 2019 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
